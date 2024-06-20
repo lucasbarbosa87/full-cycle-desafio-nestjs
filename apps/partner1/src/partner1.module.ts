d80ffb4f7331cfec35a946318e5b072d8b9fa001
lucas barbosa <lucas.barbosa.santos@outlook.com>
2024-06-20 14:08:50 -0300

    Alterado o .env

--- apps/partner1/src/partner1.module.ts
+++ apps/partner1/src/partner1.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
 import { PrismaModule } from '@app/core/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, EventsModule, SpotsModule],
+  imports: [
+    PrismaModule,
+    EventsModule,
+    SpotsModule,
+    ConfigModule.forRoot({ envFilePath: '.env.partner1', isGlobal: true }),
+  ],
 })
 export class Partner1Module {}
